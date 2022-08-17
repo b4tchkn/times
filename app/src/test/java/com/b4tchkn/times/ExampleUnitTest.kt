415df7a8a0ccb73ab5b9439d59a0d3f43e16cd15
b4tchkn <baaaakkrad@gmail.com>
2022-08-17 23:06:32 +0900

    chore: run ktlint

--- app/src/test/java/com/b4tchkn/times/ExampleUnitTest.kt
+++ app/src/test/java/com/b4tchkn/times/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.b4tchkn.times
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
